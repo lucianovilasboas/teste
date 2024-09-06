c8ccafaede5d21ef7974ad4b459d450e0c6e3a39
Luciano <lucianovilasboas@gmail.com>
2024-09-05 22:09:10 -0300

    Commit via agenda em 2024-09-05 22:09:10

--- agenda.py
+++ agenda.py
@@ -1,14 +1,24 @@
 import schedule
 import subprocess
 import time
+from datetime import datetime
 
-# Função para executar o script automacao.py
+# Função para executar o script gitrun.py
 def executar():
-    print("Executando gitrun.py...")    
-    subprocess.run(["python", "gitrun.py", "-m", "Commit via agenda"], shell=True)
+    # Obtendo o horário atual
+    horario_atual = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    # Mensagem de commit com o horário de execução
+    mensagem_commit = f"Commit via agenda em {horario_atual}"
+    
+    print(f"Executando gitrun.py com a mensagem: {mensagem_commit}")
+    
+    # Executando o script gitrun.py com a mensagem de commit
+    subprocess.run(["python", "gitrun.py", "-m", mensagem_commit], shell=True)
 
 if __name__ == "__main__":
     print("Agendador de tarefas iniciado...")
+    
     # Agendar as tarefas
     schedule.every().minute.do(executar)
 
